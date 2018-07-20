2eff924b66c0b00b0459e6baea8f0b87b56d46f2
Nikhil-GIT <salwenikhil0724@gmail.com>
2018-07-20 07:39:44 -0700

    parsing package code

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-var data = require('./main');
+/*
+ * Primary file for API
+ *
+ */
+
+// Dependencies
+var http = require('http');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+
+ // Configure the server to respond to all requests with a string
+var server = http.createServer(function(req,res){
+
+  // Parse the url
+  var parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  var queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  var method = req.method.toLowerCase();
+
+  //get the headers as an objectc
+
+  var headers = req.headers;
+
+  //Get the payload if, any
+
+  var decoder = new StringDecoder('utf-8');
+  var buffer = '';
+
+  req.on('data', function(data){
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', function(){
+    buffer += decoder.end();
+
+
+  // Send the response
+  res.end('Hello World!\n');
+
+  // Log the request/response
+  console.log('Request received received with eaders:--  ', buffer);
+
+  });
+
+
+});
+
+// Start the server
+server.listen(3000,function(){
+  console.log('The server is up and running now');
+});
